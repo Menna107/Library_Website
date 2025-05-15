e559a2bea0eac566e66310ee0a5915cdb9c9826c
Menna Hekal <145789782+Menna107@users.noreply.github.com>
2025-05-15 19:35:18 +0300

    Add files via upload

--- templates/urls.py
+++ templates/urls.py
@@ -1,13 +1,16 @@
+# urls.py
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('header', views.header, name='header'),
-    path('index', views.index, name='index'),
-    path('book-details', views.bookdetails, name='bookdetails'),
-    path('add-new-book', views.addnewbook, name='addnewbook'),
-    path('borrow-books', views.borrowbook, name='borrowbook'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
-    path('murder-book', views.murederbook, name='murderbook'),
-    path('my-books', views.mybooks, name='mybooks'),
+    path('header/', views.header, name='header'),
+    path('index/', views.index, name='index'),
+    path('book-details/<int:book_id>/', views.bookdetails, name='bookdetails'),
+    path('add-new-book/', views.addnewbook, name='addnewbook'),
+    path('borrow-books/', views.borrowbook, name='borrowbook'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('murder-book/', views.murderbook, name='murderbook'),
+    path('my-books/', views.my_books, name='my_books'),
+    path('borrow-book/<int:book_id>/', views.borrow_book, name='borrow_book'),
 ]

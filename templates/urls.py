6466ed16e46945f21db7d46af44b96fdc65c9d63
Menna Hekal <145789782+Menna107@users.noreply.github.com>
2025-05-16 18:26:41 +0300

    Add files via upload

--- templates/urls.py
+++ templates/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('borrow-books/', views.borrowbook, name='borrowbook'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout', views.user_logout, name='logout'),
     path('murder-book/', views.murderbook, name='murderbook'),
     path('my-books/', views.my_books, name='my_books'),
     path('borrow-book/<int:book_id>/', views.borrow_book, name='borrow_book'),

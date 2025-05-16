7404d694b56a1a41f46f6af7c75ed68a20699479
Menna Hekal <145789782+Menna107@users.noreply.github.com>
2025-05-16 18:28:04 +0300

    Add files via upload

--- theProject/settings.py
+++ theProject/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-LOGIN_URL = '/login/'
+LOGIN_URL = 'login'  
+LOGIN_REDIRECT_URL = 'index'  
+LOGOUT_REDIRECT_URL = 'index' 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

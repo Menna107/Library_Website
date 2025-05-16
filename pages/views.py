6ae3bcc1041188cb2e3607c54ac7bbe04f3e5e7f
malaksabry15 <148697224+malaksabry15@users.noreply.github.com>
2025-05-16 18:26:36 +0300

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -76,3 +76,11 @@ def borrow_book(request, book_id):
 def my_books(request):
     borrowed_books = Borrow.objects.filter(user=request.user).select_related('book')
     return render(request, 'my-books.html', {'borrowed_books': borrowed_books})
+
+
+@login_required
+
+def delete_borrow(request, borrow_id):
+    borrow = get_object_or_404(Borrow, id=borrow_id, user=request.user)
+    borrow.delete()
+    return redirect('my_books')
